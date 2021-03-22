f6aff470a8f9341d0cff314e639c4c0daf65f6c6
nusoil <nusoil.sales@gmail.com>
2021-03-23 00:46:18 +0800

    init

---
+++ src/com/company/Main.java
@@ -0,0 +1,27 @@
+package com.company;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.List;
+
+public class Main {
+
+    private static final String txtCSV = "./";
+
+
+    public static void main(String[] args) throws IOException {
+
+        final Path path = Paths.get(txtCSV + "test.csv");
+        final List<String> lines = Files.readAllLines(path);
+        double total = 0d;
+
+        for(final String line: lines) {
+            final String[] columns = line.split(",");
+            final double amount = Double.parseDouble(columns[1]);
+            total += amount;
+        }
+        System.out.println("total transations is:" + total);
+    }
+}
