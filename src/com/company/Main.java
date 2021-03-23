0665d5713324fe79ee1f5d9ab3c740bf0b0e8332
nusoil <nusoil.sales@gmail.com>
2021-03-23 19:23:14 +0800

    srp
    method level-srp

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -7,26 +7,41 @@ import java.nio.file.Paths;
 import java.time.LocalDate;
 import java.time.Month;
 import java.time.format.DateTimeFormatter;
+import java.util.ArrayList;
 import java.util.List;
 
 public class Main {
     private static final String txtCSV = "./";
 
     public static void main(String[] args) throws IOException {
-        double total = 0d;
-        double totalInJaunary = 0;
+
+
         List<InfoDataModel> data = new CSVParser().CSVParse(txtCSV + args[0]);
 
-        for (InfoDataModel dateItem : data) {
-            total += dateItem.getAmount();
+        System.out.println("total transations is:" + calcaulateTotalAmount(data));
 
-            if (dateItem.getDate().getMonth() == Month.JANUARY) {
-                totalInJaunary += dateItem.getAmount();
-            }
-        }
-
-        System.out.println("total transations is:" + total);
+        double totalInJaunary = calcaulateTotalAmount(selectInMonth(data, Month.JANUARY));
         System.out.println("total transations is January:" + totalInJaunary);
 
     }
+
+    public static double calcaulateTotalAmount(final List<InfoDataModel> data) {
+        double total = 0d;
+        for (InfoDataModel dateItem : data) {
+            total += dateItem.getAmount();
+        }
+        return total;
+    }
+
+    public static  List<InfoDataModel> selectInMonth(final List<InfoDataModel> data, final Month month) {
+        final List<InfoDataModel> dataInMonth = new ArrayList<>();
+        for(final InfoDataModel infoDataModel: data) {
+            if (infoDataModel.getDate().getMonth() == month) {
+                dataInMonth.add(infoDataModel);
+            }
+        }
+        return dataInMonth;
+    }
+
+
 }
